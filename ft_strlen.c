1a6b3a15cb9fb0f33c2789575c3c85175587e85e
Seungmin Kim <seungmki@c1r8s1.42seoul.kr>
2020-09-28 13:49:21 +0900

    libft

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,32 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seungmki <seungmki@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/28 12:36:04 by seungmki          #+#    #+#             */
-/*   Updated: 2020/09/28 13:48:13 by seungmki         ###   ########.fr       */
+/*   Created: 2020/09/28 13:39:50 by seungmki          #+#    #+#             */
+/*   Updated: 2020/09/28 13:47:05 by seungmki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(const char *s)
+size_t	ft_strlen(const char *s)
 {
-	long long	res;
-	int			minus;
+	size_t len;
 
-	res = 0;
-	minus = 1;
-	while (*s == ' ' || (*s >= 9 && *s <= 13))
-		s++;
-	if (*s == '+' || *s == '-')
-		minus = *s++ == '-' ? -1 : 1;
-	while (*s >= '0' && *s <= '9')
-	{
-		res = res * 10 + (*s - '0');
-		s++;
-	}
-	if (res == 2147483648 && minus == -1)
-		return (-2147483648);
-	return ((int)res * minus);
+	len = 0;
+	while (*s++ != 0)
+		len++;
+	return (len);
 }
