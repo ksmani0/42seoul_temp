397e3a2df5afe4891fde738b15a3d2073b37c714
Seungmin Kim <seungmki@c1r8s1.42seoul.kr>
2020-09-30 17:51:04 +0900

    libft

---
+++ ft_strmapi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungmki <seungmki@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/30 17:15:39 by seungmki          #+#    #+#             */
+/*   Updated: 2020/09/30 17:25:04 by seungmki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*res;
+	char			*p;
+	size_t			len;
+	unsigned int	i;
+
+	if (s == 0)
+		return ((void*)0);
+	len = ft_strlen(s);
+	res = (char*)malloc(sizeof(char) * len + 1);
+	if (res == 0)
+		return ((void*)0);
+	i = 0;
+	p = res;
+	while (*s != 0)
+	{
+		*res = f(i++, *s);
+		res++;
+		s++;
+	}
+	*res = 0;
+	return (p);
+}
