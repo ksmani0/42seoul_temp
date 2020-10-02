2ba38ae165e7c6c827965fc6aca3d87715cd062e
Seungmin Kim <seungmki@c1r8s1.42seoul.kr>
2020-10-02 22:26:32 +0900

    libft

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: seungmki <seungmki@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/02 11:52:53 by seungmki          #+#    #+#             */
-/*   Updated: 2020/10/02 14:51:11 by seungmki         ###   ########.fr       */
+/*   Updated: 2020/10/02 22:22:11 by seungmki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	t_list **temp;
 
 	if (lst == 0 || new == 0)
-		return (NULL);
+		return ;
 	temp = lst;
 	new->next = *temp;
 	lst = &new;
