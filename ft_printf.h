0b6e8eacb55b591806d40b956ef3d4b09951e466
Seungmin Kim <seungmki@c4r10s2.42seoul.kr>
2020-10-30 17:39:08 +0900

    test

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: seungmki <seungmki@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 13:06:17 by seungmki          #+#    #+#             */
-/*   Updated: 2020/10/19 13:10:42 by seungmki         ###   ########.fr       */
+/*   Updated: 2020/10/30 17:38:22 by seungmki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
-//static char	s_hex_b[17] = "0123456789ABCDEF";
-//static char	s_hex_s[17] = "0123456789abcdef";
+char	g_hex_b[17] = "0123456789ABCDEF";
+char	g_hex_s[17] = "0123456789abcdef";
 
 char		*g_double[52];
-g_double[0] = "500000000";
+//g_double[0] = "500000000";
 
-typedef struct s_format {
+typedef struct	s_format
+{
 	char	*str;
 	char	flag[5];
 	char	spec;
@@ -34,18 +35,18 @@ typedef struct s_format {
 	char	*width;
 	char	*prec;
 	short	out_len;
-	int	out_num;
-}		t_format
+	int		out_num;
+}			t_format;
 
 /*
 *****ft_printf.c*****
 */
 
 t_format	*initial_malloc(t_format **list);
-void		print_char(t_format* list);
-int		if_flag_check(t_format *list);
+void		print_char(t_format *list);
+int			if_flag_check(t_format *list);
 void		str_or_format(va_list ap, t_format *list);
-int		ft_printf(const char *format, ...);
+int			ft_printf(const char *format, ...);
 
 /*
 *****check_func.c*****
@@ -58,16 +59,16 @@ int		ft_printf(const char *format, ...);
 size_t		count_num_len(long num, t_format *list);
 void		input_list_num(t_format *list, long num, size_t len);
 void		output_int(char *out, t_format *list, size_t len);
-int		print_s_int(t_format *list);
+int			print_s_int(t_format *list);
 
 /*
 *****print_uint.c*****
 */
 
-int		print_s_uint(t_format *list);
-int		print_s_char(t_format *list);
-size_t		parse_input_hex(unsigend int x, t_format *list);
-int		print_s_octal(t_format *list);
+int			print_s_uint(t_format *list);
+int			print_s_char(t_format *list);
+size_t		parse_input_hex(unsigned int x, t_format *list);
+int			print_s_octal(t_format *list);
 void		print_s_number(t_format *list);
 
 #endif
