05ce74f7174f694227d243bcadbd471c385558b0
Seungmin Kim <seungmki@c5r9s6.42seoul.kr>
2021-03-16 23:17:17 +0900

    need to check

---
+++ check/srcs/db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS 'seungmki'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'seungmki'@'localhost' WITH GRANT OPTION;
