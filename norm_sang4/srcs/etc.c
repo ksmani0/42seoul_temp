dd32b6301fb37600b408b03b372b6bc50a3e7327
김승민 <ksmani0@gmail.com>
2021-07-30 06:26:07 +0900

    norm check

---
+++ norm_sang4/srcs/etc.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   etc.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghpar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/28 19:17:09 by sanghpar          #+#    #+#             */
+/*   Updated: 2020/11/19 14:00:25 by sanghpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	**make_argv(t_cmd *c_list, char *buf)
+{
+	int		i;
+	char	**tmp;
+	t_list	*cmd;
+
+	cmd = c_list->cmd->next;
+	i = 0;
+	while (cmd)
+	{
+		cmd = cmd->next;
+		i++;
+	}
+	tmp = (char **)malloc(sizeof(char *) * (i + 2));
+	tmp[i + 1] = 0;
+	i = 1;
+	tmp[0] = ft_strdup(buf);
+	cmd = c_list->cmd->next;
+	while (cmd)
+	{
+		tmp[i] = ft_strdup(cmd->content);
+		cmd = cmd->next;
+		i++;
+	}
+	return (tmp);
+}
+
+void	execute_etc(t_cmd *c_list, char *buf)
+{
+	int		status;
+	char	**argv;
+	pid_t	pid;
+
+	g_data->forked = 1;
+	tcsetattr(STDIN_FILENO, TCSANOW, &g_data->child_term);
+	pid = fork();
+	if (pid == 0)
+	{
+		set_pipe(c_list);
+		set_rd(c_list->r_list);
+		argv = make_argv(c_list, buf);
+		if (execve(buf, argv, g_data->envp) < 0)
+		{
+			printf("bash: %s: %s\n", c_list->cmd->content, strerror(errno));
+			if (errno == 13)
+				exit(126);
+			else if (errno == 2)
+				exit(127);
+			free_split(argv);
+		}
+	}
+	else
+		shit_norm_1(pid, status, c_list);
+}
+
+void	shit_norm_1(int pid, int status, t_cmd *c_list)
+{
+	waitpid(pid, &status, 0);
+	close(c_list->fds[1]);
+	if (WIFEXITED(status))
+		g_data->ret = WEXITSTATUS(status);
+	tcsetattr(STDIN_FILENO, TCSANOW, &g_data->main_term);
+}
+
+int	check_executable(char *buf, t_cmd *c_list)
+{
+	int			i;
+	struct stat	sb;
+
+	i = stat(buf, &sb);
+	if (i != 0)
+	{
+		g_data->ret = 127;
+		return (1);
+	}
+	else if (S_ISDIR(sb.st_mode))
+	{
+		g_data->ret = 126;
+		return (2);
+	}
+	else
+	{
+		if (sb.st_mode & S_IXUSR)
+			return (0);
+		else
+		{
+			g_data->ret = 999;
+			return (3);
+		}
+	}
+}
+
+void	etc(t_cmd *c_list)
+{
+	char	*cmd;
+	char	buf[5000];
+	int		i;
+
+	cmd = c_list->cmd->content;
+	if (*cmd != '/')
+	{
+		if (check_current_folder(c_list))
+			return ;
+		else if (check_path_folder(c_list))
+			return ;
+		else
+			print_error(c_list);
+	}
+	else
+	{
+		if (check_executable(c_list->cmd->content, c_list) == 0)
+			execute_etc(c_list, c_list->cmd->content);
+		else
+			print_error(c_list);
+	}
+}
