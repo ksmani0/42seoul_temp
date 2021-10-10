6f938aecafa6d841243590894becc1daaa228de6
Seungmin Kim <seungmki@c7r4s7.42seoul.kr>
2021-10-10 21:16:33 +0900

    final cpp00

---
+++ cp00/ex01/main.cpp
@@ -0,0 +1,26 @@
+#include "PhoneBook.hpp"
+
+int main(void)
+{    
+    std::string input;
+    PhoneBook phonebook;
+
+    while (true)
+    {
+        std::cout << "Please, Choose a command from ADD, SEARCH, or EXIT:\n";
+        std::cin >> input;
+
+        if (input == "EXIT")
+            break;
+        else if (input == "SEARCH")
+        {
+            phonebook.ShowAllList();
+            phonebook.ShowOneContact();
+        }
+        else if (input == "ADD")
+            phonebook.AddList();
+        else
+            std::cout << "Wrong command! Input a right command.\n";
+    }    
+    return 0;
+}
