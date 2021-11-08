f855d5ad39299f814454b58da3953107b96b615f
김승민 <ksmani0@gmail.com>
2021-11-09 00:41:08 +0900

    checking cpp04

--- intra/ex03/src/main.cpp
+++ intra/ex03/src/main.cpp
@@ -1,25 +1,26 @@
-#include "DiamondTrap.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
+#include "MateriaSource.hpp"
+#include "Character.hpp"
 
 int main(void)
 {
-    DiamondTrap kim("kim");
-	DiamondTrap lee("lee");
-	std::cout << std::endl;
+    IMateriaSource* src = new MateriaSource();//mInventory 모두 널 초기화
+    src->learnMateria(new Ice());//[0]은 ice 대입 받음
+    src->learnMateria(new Cure());//[1]은 cure 대입 받음
+    ICharacter* me = new Character("me");//mName은 me, mInventory은 모두 널
+    AMateria* tmp;
 
-	kim.whoAmI();
-	lee.whoAmI();
-	std::cout << std::endl;
+    tmp = src->createMateria("ice");//[0] clone() 반환
+    me->equip(tmp);//[0]에 대입 받음
+    tmp = src->createMateria("cure");
+    me->equip(tmp);//[1]에 대입 받음
 
-	kim.attack("lee");
-	lee.takeDamage(kim.GetAttackDamage());
-	std::cout << std::endl;
+    ICharacter* bob = new Character("bob");//mName은 bob, mInventory은 모두 널
+    me->use(0, *bob);//ice
+    me->use(1, *bob);//cure
 
-	lee.attack("kim");
-	kim.takeDamage(lee.GetAttackDamage());
-	std::cout << std::endl;
-
-	kim.guardGate();
-	lee.highFivesGuys();
-
-	return (0);
+    delete bob; delete me; delete src;
+    system("leaks interface");
+    return 0;
 }
