fe85809baf77848b9be60a38b39973dfd88670f0
김승민 <ksmani0@gmail.com>
2021-11-09 13:15:01 +0900

    checking cpp05

--- intra/ex03/src/main.cpp
+++ intra/ex03/src/main.cpp
@@ -1,26 +1,40 @@
-#include "Ice.hpp"
-#include "Cure.hpp"
-#include "MateriaSource.hpp"
-#include "Character.hpp"
+#include "Intern.hpp"
 
 int main(void)
 {
-    IMateriaSource* src = new MateriaSource();//mInventory 모두 널 초기화
-    src->learnMateria(new Ice());//[0]은 ice 대입 받음
-    src->learnMateria(new Cure());//[1]은 cure 대입 받음
-    ICharacter* me = new Character("me");//mName은 me, mInventory은 모두 널
-    AMateria* tmp;
+    Bureaucrat kim("kim", 3);
+    Intern lee;//매개변수 없는 생성자 lee() 쓰면 함수 선언처럼 되니까 쓰면 안 됨
 
-    tmp = src->createMateria("ice");//[0] clone() 반환
-    me->equip(tmp);//[0]에 대입 받음
-    tmp = src->createMateria("cure");
-    me->equip(tmp);//[1]에 대입 받음
+    Form* robotomy = lee.makeFome("robotomy request", "Bender");
+    kim.signForm(*robotomy);
+    kim.executeForm(*robotomy);
 
-    ICharacter* bob = new Character("bob");//mName은 bob, mInventory은 모두 널
-    me->use(0, *bob);//ice
-    me->use(1, *bob);//cure
+    std::cout << "\n\n";
+    Form* shrubbery = lee.makeFome("shrubbery creation", "home");
+    kim.signForm(*shrubbery);
+    kim.executeForm(*shrubbery);
 
-    delete bob; delete me; delete src;
-    system("leaks interface");
+    std::cout << "\n\n";
+    Form* president = lee.makeFome("presidential pardon", "corruption officials");
+    kim.signForm(*president);
+    kim.executeForm(*president);
+
+    delete robotomy; delete shrubbery; delete president;
+
+    std::cout << "\n\n";
+    Form* nullPtr = lee.makeFome("x-form", "target");//존재하지 않는 Form 넣어서 예외 뜸
+    if (nullPtr == NULL)//널 예외는 c++가 아닌 OS상 에러라서 try~catch 무소용
+        std::cerr << "Null exception!\n";
     return 0;
 }
+
+/*
+try
+    {
+        Form* nullPtr = lee.makeFome("x-form", "target");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Null exception!";
+    }
+*/
